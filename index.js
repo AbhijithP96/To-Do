9cd975fc3532982b9d1434fec24308dcd0aeae3e
AbhijithP96 <premraj.abhijith@gmail.com>
2023-10-15 17:07:56 +0200

    Version 1.1

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var task_num = 1;
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", (req,res) => {
-    res.render("index.ejs");
-} )
+    res.render("index.ejs",{run : task_num});
+} );
+
+app.post("/plus", (req,res) => {
+    task_num++;
+    res.render("index.ejs", {run : task_num});
+
+});
 
 app.listen(port , () => {
     console.log(`Listening on port ${port}`);
-})
+});
